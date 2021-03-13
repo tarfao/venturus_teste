85ecf50c49526448973b3cec877a76c57b32888c
tarfao <tarfao@hotmail.com>
2021-03-12 21:53:19 -0300

    adicionando o footer

--- src/index.js
+++ src/index.js
@@ -6,9 +6,18 @@ ReactDOM.render(
   <React.StrictMode>
     <header>
       <div className='container-header-title-logo'>
-        <h2>Squad Management Tool</h2>
+        <h1>Squad Management Tool</h1>
+      </div>
+      <div className='container-header-usuario'>
+        <h2>John Doe</h2>
+        <div className='imagem-usuario'>
+          <h2>JD</h2>
+        </div>
       </div>
     </header>
+    <footer>
+      2020 - All rights reserved
+      </footer>
   </React.StrictMode>,
   document.getElementById('root')
 );
