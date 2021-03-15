eb064d9a01fafad91691ea8f318133dc3e10bb7a
tarfao <tarfao@hotmail.com>
2021-03-15 09:46:34 -0300

    adicionando containers para os componentes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@ import './index.css';
 
 ReactDOM.render(
   <ContainerNavFooter>
-    <div>Olar</div>
+    <div className='content'>
+      <div className='my-teams-container'>
+
+      </div>
+      <div className='top5-most-less-picked-container'>
+        <div className='top5-container'>
+
+        </div>
+        <div className='moss-less-picked-container'>
+          
+        </div>
+      </div>
+    </div>
   </ContainerNavFooter>,
   document.getElementById('root')
 );
